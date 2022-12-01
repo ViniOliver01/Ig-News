d90231b9597f84f200f7395fd5bcf5e45cb0f5b9
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-12-01 17:13:09 -0300

    Updated SignIn Button

--- src/components/SignInButton/SignInButton.tsx
+++ src/components/SignInButton/SignInButton.tsx
@@ -1,30 +1,22 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-import styles from './SignInButton.module.scss'
+import styles from "./SignInButton.module.scss";
 
-export default function SignInButton(){
-  const {data: session} = useSession()
-  console.log("🚀 / SignInButton / session", session)
+export default function SignInButton() {
+  const { data: session } = useSession();
 
-  return session? (
-    <button
-      type='button'
-      className={styles.signInButton}
-    >
-        <FaGithub color='#04d361'/> 
-        {session.user.name}
-        <FiX color='#737380' className={styles.closeIcon} onClick={() => signOut()}/> 
+  return session ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
     </button>
   ) : (
-    <button
-      type='button'
-      className={styles.signInButton}
-      onClick={() => signIn('github')}
-    >
-        <FaGithub color='#eba417'/> 
-        Sign In with GitHub
+    <button type="button" className={styles.signInButton} onClick={() => signIn("github")}>
+      <FaGithub color="#eba417" />
+      Sign In with GitHub
     </button>
-  )
+  );
 }
