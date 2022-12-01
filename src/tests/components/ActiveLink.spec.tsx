bab57485c0af2d61202e7b6ee4a79ed51ddf47dc
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-12-01 17:13:39 -0300

    Updated Path

--- src/tests/components/ActiveLink.spec.tsx
+++ src/tests/components/ActiveLink.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import ActiveLink from "../../../components/ActiveLink/ActiveLink";
+import ActiveLink from "../../components/ActiveLink/ActiveLink";
 
 jest.mock("next/router", () => {
   return {
--- src/tests/components/Header.spec.tsx
+++ src/tests/components/Header.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Header from "./../../../components/Header/Header";
+import Header from "../../components/Header/Header";
 
 jest.mock("next/router", () => {
   return {
