620d2347b76a616110f7f63b3765d97be9c738a6
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-12-05 10:26:26 -0300

    organized page code

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head';
-import { GetServerSideProps } from 'next';
-import styles from '../../styles/pages/slug.module.scss'
-import { getPrismicClient } from '../../services/prismic';
-import { RichText } from 'prismic-dom';
-import { getSession } from 'next-auth/react';
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+import styles from "../../styles/pages/slug.module.scss";
+import { getPrismicClient } from "../../services/prismic";
+import { RichText } from "prismic-dom";
+import { getSession } from "next-auth/react";
 
 interface PostProps {
-  post: Post
+  post: Post;
 }
 
 interface Post {
@@ -26,110 +26,103 @@ interface Content {
   dimensions?: {
     height: number;
     width: number;
-  }
+  };
   // Iframes
-  oembed?:{
+  oembed?: {
     src?: string;
     title?: string;
     html?: string;
-  }
-  
+  };
 }
 
-export default function Post({post}: PostProps){
-
+export default function Post({ post }: PostProps) {
   return (
     <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
-    
-        <div className={styles.container}>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-          <article className={styles.post}>
-            <h1>{post.title}</h1>
-            <time>{post.updatedAt}</time>
-          </article>
+      <div className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+        </article>
 
-          <div className={styles.postContent}>
+        <div className={styles.postContent}>
+          {post.content.map((item, index) => {
+            if (item.type == "paragraph") {
+              return <p key={index}>{item.text}</p>;
+            }
+            if (item.type == "heading2") {
+              return <h2 key={index}>{item.text}</h2>;
+            }
+            if (item.type == "heading3") {
+              return <h3 key={index}>{item.text}</h3>;
+            }
 
-
-                {post.content.map((item, index) => {
-
-                  if(item.type == 'paragraph'){
-                    return <p key={index}>{item.text}</p>
-                  }
-                  if(item.type == 'heading2'){
-                    return <h2 key={index}>{item.text}</h2>
-                  }
-                  if(item.type == 'heading3'){
-                    return <h3 key={index}>{item.text}</h3>
-                  }
-                  
-                  if(item.type == 'heading6'){
-                    return <cite key={index}>{item.text}</cite>
-                  }
-                  if(item.type == 'list-item'){
-                    return <li key={index}>{item.text}</li>
-                  }
-                  if(item.type == 'image'){
-                    return <img 
-                            key={index}
-                            src={item.url} 
-                            alt={item.alt}
-                            width={item.dimensions.width}
-                            height={item.dimensions.height}
-                            />
-                  }
-                  if(item.type == 'embed'){
-                    return <iframe 
-                            key={index}
-                            height={400} 
-                            src={item.oembed.html.split(`"`)[5]}
-                            title="Prisma 2: Automatize o acesso ao banco de dados | Code/Drops #29"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                  }
-
-
-
-
-                })}
-            </div>
+            if (item.type == "heading6") {
+              return <cite key={index}>{item.text}</cite>;
+            }
+            if (item.type == "list-item") {
+              return <li key={index}>{item.text}</li>;
+            }
+            if (item.type == "image") {
+              return (
+                <img
+                  key={index}
+                  src={item.url}
+                  alt={item.alt}
+                  width={item.dimensions.width}
+                  height={item.dimensions.height}
+                />
+              );
+            }
+            if (item.type == "embed") {
+              return (
+                <iframe
+                  key={index}
+                  height={400}
+                  src={item.oembed.html.split(`"`)[5]}
+                  title="Prisma 2: Automatize o acesso ao banco de dados | Code/Drops #29"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                ></iframe>
+              );
+            }
+          })}
         </div>
+      </div>
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
-  const {slug} = params
-  const session = await getSession({req})
-  console.log("🚀 / constgetServerSideProps:GetServerSideProps= / session", session)
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const { slug } = params;
+  const session = await getSession({ req });
 
-  if(!session?.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
-      }
-    }
+      },
+    };
   }
 
-  
-  const prismic = getPrismicClient(req)
+  const prismic = getPrismicClient(req);
   const response = await prismic.getByUID("posts", String(slug));
-  
+
   const post = {
     title: RichText.asText(response.data.title),
-    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
-              day: '2-digit',
-              month: 'long',
-              year: 'numeric',  
-          }),
-    content: response.data.content
-  }
-  return{
-    props:{
-      post
-    }
-  }
-}
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    }),
+    content: response.data.content,
+  };
+  return {
+    props: {
+      post,
+    },
+  };
+};
--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,15 +1,15 @@
-import Head from 'next/head';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import styles from '../../../styles/pages/slug.module.scss'
-import { getPrismicClient } from '../../../services/prismic';
-import { RichText } from 'prismic-dom';
-import Link from 'next/link';
-import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import { GetStaticPaths, GetStaticProps } from "next";
+import styles from "../../../styles/pages/slug.module.scss";
+import { getPrismicClient } from "../../../services/prismic";
+import { RichText } from "prismic-dom";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface PostPreviewProps {
-  post: Post
+  post: Post;
 }
 
 interface Post {
@@ -30,119 +30,116 @@ interface Content {
   dimensions?: {
     height: number;
     width: number;
-  }
+  };
   // Iframes
-  oembed?:{
+  oembed?: {
     src?: string;
     title?: string;
     html?: string;
-  }
-  
+  };
 }
 
-export default function PostPreview({post}: PostPreviewProps){
-console.log("🚀 / PostPreview / post", post)
-const {data: session} = useSession()
-const router = useRouter()
+export default function PostPreview({ post }: PostPreviewProps) {
+  const session = useSession();
+  const router = useRouter();
 
-useEffect(() => {
-  if(session?.activeSubscription){
-    router.push(`/posts/${post.slug}`)
-  }
-},[session])
+  useEffect(() => {
+    if (session?.data?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session]);
 
   return (
     <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
-    
-        <div className={styles.container}>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-          <article className={styles.post}>
-            <h1>{post.title}</h1>
-            <time>{post.updatedAt}</time>
-          </article>
+      <div className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+        </article>
 
-            <div className={`${styles.postContent} ${styles.previewContent}`}>
+        <div className={`${styles.postContent} ${styles.previewContent}`}>
+          {post.content.map((item, index) => {
+            if (item.type == "paragraph") {
+              return <p key={index}>{item.text}</p>;
+            }
+            if (item.type == "heading2") {
+              return <h2 key={index}>{item.text}</h2>;
+            }
+            if (item.type == "heading3") {
+              return <h3 key={index}>{item.text}</h3>;
+            }
 
-                {post.content.map((item, index) => {
-
-                  if(item.type == 'paragraph'){
-                    return <p key={index}>{item.text}</p>
-                  }
-                  if(item.type == 'heading2'){
-                    return <h2 key={index}>{item.text}</h2>
-                  }
-                  if(item.type == 'heading3'){
-                    return <h3 key={index}>{item.text}</h3>
-                  }
-                  
-                  if(item.type == 'heading6'){
-                    return <cite key={index}>{item.text}</cite>
-                  }
-                  if(item.type == 'list-item'){
-                    return <li key={index}>{item.text}</li>
-                  }
-                  if(item.type == 'image'){
-                    return <img 
-                            key={index}
-                            src={item.url} 
-                            alt={item.alt}
-                            width={item.dimensions.width}
-                            height={item.dimensions.height}
-                            />
-                  }
-                  if(item.type == 'embed'){
-                    return <iframe 
-                            key={index}
-                            height={400} 
-                            src={item.oembed.html.split(`"`)[5]}
-                            title="Prisma 2: Automatize o acesso ao banco de dados | Code/Drops #29"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                  }
-
-                })}
-                <div className={styles.continueReading}>
-                  Wanna continue reading?
-                  <Link href='#'> Subscribe now 🤗</Link>
-                </div>
-            </div>
+            if (item.type == "heading6") {
+              return <cite key={index}>{item.text}</cite>;
+            }
+            if (item.type == "list-item") {
+              return <li key={index}>{item.text}</li>;
+            }
+            if (item.type == "image") {
+              return (
+                <img
+                  key={index}
+                  src={item.url}
+                  alt={item.alt}
+                  width={item.dimensions.width}
+                  height={item.dimensions.height}
+                />
+              );
+            }
+            if (item.type == "embed") {
+              return (
+                <iframe
+                  key={index}
+                  height={400}
+                  src={item.oembed.html.split(`"`)[5]}
+                  title="Prisma 2: Automatize o acesso ao banco de dados | Code/Drops #29"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                ></iframe>
+              );
+            }
+          })}
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="#"> Subscribe now 🤗</Link>
+          </div>
         </div>
+      </div>
     </>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
-  return{
+  return {
     paths: [],
-    fallback: 'blocking'
-  }
+    fallback: "blocking",
+  };
+};
 
-}
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-  const {slug} = params
-
-  
-  const prismic = getPrismicClient()
+  const prismic = getPrismicClient();
   const response = await prismic.getByUID("posts", String(slug));
-  
+
   const post = {
     slug: response.uid,
     title: RichText.asText(response.data.title),
-    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
-              day: '2-digit',
-              month: 'long',
-              year: 'numeric',  
-          }),
-    content: response.data.content.splice(0,3)
-  }
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    }),
+    content: response.data.content.splice(0, 3),
+  };
 
-  return{
-    props:{
-      post
+  return {
+    props: {
+      post,
     },
-    revalidate: 60 * 60 * 24 //24 Hours
-  }
-}
+    revalidate: 60 * 60 * 24, //24 Hours
+  };
+};
