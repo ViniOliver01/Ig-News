e689ddf50de07d7563ecf4d57f617a6ce4dcee44
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-12-05 10:26:46 -0300

    organized component code

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,45 +1,36 @@
-import styles from './SubscribeButton.module.scss';
-import { useSession, signIn } from 'next-auth/react';
-import { api } from './../../services/api';
-import { getStripeJs } from '../../services/stripe-js';
-import { useRouter } from 'next/router';
+import styles from "./SubscribeButton.module.scss";
+import { useSession, signIn } from "next-auth/react";
+import { api } from "./../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import { useRouter } from "next/router";
 
-interface SubscribeButtonProps{
-  priceId: string
-}
+export default function SubscribeButton() {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-export default function SubscribeButton({priceId}: SubscribeButtonProps){
-  const { data: session } = useSession()
-  const router = useRouter()
-
-  async function handleSubscribe(){
-    if(!session){
-      signIn('github')
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
       return;
     }
 
-    if(session.activeSubscription){
-      router.push('/posts')
+    if (session.activeSubscription) {
+      router.push("/posts");
       return;
     }
 
-    try{
-      const response = await api.post('/subscribe')
-      const { sessionId } = response.data
-      const stripe = await getStripeJs()
-      await stripe.redirectToCheckout({sessionId})
-    } catch(err) {
-      alert(err.message)
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
-
   }
 
   return (
-    <button 
-      type='button'
-      className={styles.subscribeButton}
-      onClick={handleSubscribe}
-    >
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe Now
     </button>
   );
